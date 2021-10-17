fabbfa4d56d90db92f0fd4c1cd8e10525a10752b
IvanProg00 <gavrilovivan2000@gmail.com>
2021-10-17 19:22:56 +0200

    Added new tests

--- kata02/binary_search_test.go
+++ kata02/binary_search_test.go
@@ -16,17 +16,62 @@ var tests = []struct {
 		num:      1,
 		arr:      []int{0, 1, 2},
 		expected: 1,
-	},
-	{
+	}, {
 		num:      59,
 		arr:      []int{-53, 0, -59, 53},
 		expected: -1,
+	}, {
+		num:      39,
+		arr:      []int{},
+		expected: -1,
+	}, {
+		num:      493,
+		arr:      []int{493},
+		expected: 0,
+	}, {
+		num:      -48,
+		arr:      []int{-48},
+		expected: 0,
+	}, {
+		num:      0,
+		arr:      []int{3, 0},
+		expected: 1,
+	}, {
+		num:      5,
+		arr:      []int{-3, 0, 5},
+		expected: 2,
+	}, {
+		num:      18,
+		arr:      []int{9, 3, 0},
+		expected: -1,
+	}, {
+		num:      11,
+		arr:      []int{11, 3, 0},
+		expected: 0,
+	}, {
+		num:      43,
+		arr:      []int{43, 66, 12, 65},
+		expected: 0,
+	}, {
+		num:      -52,
+		arr:      []int{43, 66, 12, 65},
+		expected: -1,
+	}, {
+		num:      -52,
+		arr:      []int{-43, -66, -12, -52},
+		expected: 3,
+	}, {
+		num:      -2,
+		arr:      []int{-43, -66, -2, -52},
+		expected: 2,
 	},
 }
 
 func TestIterativeSearch(t *testing.T) {
+	require := require.New(t)
+
 	for _, test := range tests {
 		v := kata02.IterativeSearch(test.num, test.arr)
-		require.Equal(t, v, test.expected)
+		require.Equal(test.expected, v)
 	}
 }
